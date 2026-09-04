027a95d1e24afafd2077d4db0e8c28bb8f64a342
fabricio31monteleone-sketch <fabricio31monteleone@gmail.com>
2026-09-04 18:19:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.static(path.join(__dirname)));
 
+// RUTA RAÍZ: Muestra el archivo index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.post('/api/denuncia', async (req, res) => {
   try {
     const { tipo, detalle, ubicacion, adjuntos, bcc } = req.body;
